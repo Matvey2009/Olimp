9a0fdddcbb6fcc4106f391d53f952217e3e72480
Matvey2009 <matvey2009ua@gmail.com>
2021-11-13 20:49:58 +0300

    Update 2021.3.py

--- 2021.3.py
+++ 2021.3.py
@@ -1,35 +1,25 @@
-N, M = map(int, input().split())
-arr  = []
+N, M = map(int, (input().split()))
+arr = []
+maxY = pos = 0
 
 
 for y in range(N):
-    arr.append(list(map(int, input().split())))
-
-maxY = posY = posY = tmpX = 0
-for y in range(N):
-    minX = 1000000000
-    for x in range(M):
-        if arr[y][x] < minX:
-            minX = arr[y][x]
-            tmpX = x
-    if minX > maxY:
+    row = list(map(int, input().split()))
+    arr.append(row)
+    minX = min(row)
+    if maxY < minX:
         maxY = minX
-        posY = y
-        posX = tmpX
-
-for y in range(N):
-    if maxY < arr[y][posX]:
-        maxY = 0
-        break
+        pos = y
         
-print(maxY)
-if maxY > 0:
-    print(posY, posX)
-
-#Cедловая точка- это максимальный элемент в строке и минимальный в столбце
-#5 4
-#2 3 2 5
-#1 2 1 3
-#9 8 5 6
-#3 4 3 4
-#2 1 2 3
+for x in range(M):
+    if maxY == arr[pos][x]:
+        for y in range(N):
+            if maxY < arr[y][x]:
+                break
+            if  y == N-1:
+                print(maxY)
+                print(pos, x)
+                maxY = 0
+                break
+if maxY:
+    print(0 )
