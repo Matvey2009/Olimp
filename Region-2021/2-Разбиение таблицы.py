c83a4f74b95ac11c4efe0875e5681e4f1f9c8788
Matvey2009 <matvey2009ua@gmail.com>
2022-01-04 17:15:04 +0300

    Update 2-Разбиение таблицы.py

--- Region-2021/2-Разбиение таблицы.py
+++ Region-2021/2-Разбиение таблицы.py
@@ -3,14 +3,38 @@ t = int(input())
 for i in range(t):
     n, m = map(int, input().split())
     k = n * m
-    s = int((1 + k) * k / 2) ; print('Вся сумма:', s) #Удалить#
-    ps = s / 2 ; print('Половина всей суммы:', ps)
+    s = int((1 + k) * k / 2)
+    ps= s / 2
     v =  m // 2
-    col1 = (1 + (n-1) * m + 1) * n / 2 ; print('Первая  колонка:', col1)
-    colv = (v + (n-1) * m + v) * n / 2 ; print('Средняя колонка:', colv)
-    vs = (col1 + colv) * v / 2 ; print('Сумма левой половины', vs)
-    vr = ps - vs ; print('Отклонение левой половины', vr)
-    vr2= ps - (s - vs) ; print('Отклонение правой половины', vr2)
+    col1 = (1 + (n-1) * m + 1) * n / 2
+    colv = (v + (n-1) * m + v) * n / 2
+    colv2= ((v+1) + (n-1) * m + (v+1)) * n / 2
+    vs  = (col1 + colv)  * v / 2
+    vs2 = (col1 + colv2) * (v+1) / 2
+    vr = abs(ps - vs )
+    vr2= abs(ps - vs2)
     
-    row1 = ((1 + (m+1) * n - 1) * m / 2) / m ; print('Первый ряд', row1)
-    rowv = ((m / 2) + (1 + (m+1) * n - 1) * m / 2) - k ; print('Второй ряд', row2)
+    if vr <= vr2:
+        minV = vr
+        col  = v
+    else:
+        minV = vr2
+        col  = v+1
+
+    h   = round(n * 0.70666667) #Доделать#
+    hs  = (1 + h*m) * h*m / 2
+    hs2 = (1 + (h+1)*m) * (h+1)*m / 2
+    hr = abs(ps - hs )
+    hr2= abs(ps - hs2)
+    
+    if hr <= hr2:
+        minH = hr
+        row  = h
+    else:
+        minH = hr2
+        row  = h+1
+    
+    if minV <= minH:
+        print('V', col+1)
+    else:
+        print('H', row+1)
