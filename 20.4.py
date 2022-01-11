99e7d56efc03dc2db6413cce1ca54d6327976c86
Matvey2009 <matvey2009ua@gmail.com>
2022-01-11 21:57:22 +0300

    Create 20.4.py

---
+++ 20.4.py
@@ -0,0 +1,14 @@
+N = int(input())
+arr = []
+
+for i in range((N-1) // 2):
+    arr.append([])
+    for j in range(((N-(i*2):
+        arr[i].append(j)
+
+
+T = int(input())
+for i in range(T):
+    K, S = map(int, input().split())
+    for j in range(len(arr[K-1]))
+        arr[K-1]
