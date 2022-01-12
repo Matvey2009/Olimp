cb3daa624ba96345de0caa5f7868c1f40dff8d0c
Matvey2009 <matvey2009ua@gmail.com>
2022-01-12 21:55:36 +0300

    Update 20.4.py

--- 20.4.py
+++ 20.4.py
@@ -1,14 +1,26 @@
 N = int(input())
-arr = []
-
-for i in range((N-1) // 2):
-    arr.append([])
-    for j in range(((N-(i*2):
-        arr[i].append(j)
-
-
+arr = [0] * ((N-1) // 2)
 T = int(input())
 for i in range(T):
     K, S = map(int, input().split())
-    for j in range(len(arr[K-1]))
-        arr[K-1]
+    arr[K-1] = (arr[K-1] + S) % ((N-1) * 4 - (K-1) * 8)
+
+matrix = [0] * N
+for i in range(N):
+    matrix[i] = [1] * N
+
+for i in range(len(arr)):
+    tN = N - i * 2
+    L = (tN - 1) * 4
+    for x in range(tN):
+        matrix[i][(N - tN) // 2 + x] = x+1
+        #Низ#
+        pass
+    for y in range(N):
+        #Лево#
+        #Прав#
+        pass
+
+for i in matrix:
+    print(*i)
+    
