534c17f2de22c98f9f9497d651391908887e3173
Matvey2009 <matvey2009ua@gmail.com>
2022-01-13 21:09:09 +0300

    Update 20.4.py

--- 20.4.py
+++ 20.4.py
@@ -13,13 +13,11 @@ for i in range(len(arr)):
     tN = N - i * 2
     L = (tN - 1) * 4
     for x in range(tN):
-        matrix[i][(N - tN) // 2 + x] = x+1
-        #Низ#
-        pass
-    for y in range(N):
-        #Лево#
-        #Прав#
-        pass
+         matrix[i][i + x] = (x - arr[i]) % L + 1
+         matrix[N - i - 1][i + x] = (L - (tN - 2) - x - arr[i] - 1) % L + 1
+    for y in range(tN-2):
+        matrix[y + i + 1][i] = (L - y - arr[i] - 1) % L + 1
+        matrix[y + i + 1][N - i - 1] = (tN + y - arr[i]) % L + 1
 
 for i in matrix:
     print(*i)
